575dadb2493ad8f6c75c1004fe6fe410046ec11e
Jesse Matara <jesse.matara@student.moringaschool.com>
2025-06-27 21:54:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the main element from the DOM
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Assign it the ID 'victory'
+newHeader.id = 'victory';
+
+// Set its content to include your name
+newHeader.textContent = 'Jesse Matara is the champion';
+
+// Add it to the DOM
+document.body.append(newHeader);
